52110d3a6343ea9134069fdfddd2f9b1d1c7e0a1
Abdul <abdulshafey24@gmail.com>
2024-05-04 14:32:09 +0530

    Connect to database

--- server.js
+++ server.js
@@ -1,9 +1,24 @@
 const express = require("express");
 const dotenv = require("dotenv");
+const morgan = require("morgan");
+const connectToDb = require("./config/db");
+//Route File
+const bootcamps = require("./routes/bootcamps");
 
+//Load env var
 dotenv.config({ path: "./config/config.env" });
 
+//Connect to Database
+connectToDb();
+
 const app = express();
+
+if (process.env.NODE_ENV === "development") {
+  app.use(morgan("dev"));
+}
+
+//Mount routers
+app.use("/api/v1/bootcamps", bootcamps);
 
 const PORT = process.env.PORT || 8000;
 app.listen(PORT, () =>

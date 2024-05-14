b5d432f1884089bbf96f0acd4eb55a18b576540e
Abdul <abdulshafey24@gmail.com>
2024-05-14 15:21:32 +0530

    Create Course Schema

--- seeder.js
+++ seeder.js
@@ -8,6 +8,7 @@ dotenv.config({ path: "./config/config.env" });
 
 // load models
 const Bootcamp = require("./models/Bootcamps");
+const Course = require("./models/Course");
 
 //Connect to db
 mongoose.connect(process.env.MONGO_URI);
@@ -16,11 +17,15 @@ mongoose.connect(process.env.MONGO_URI);
 const bootcamps = JSON.parse(
   fs.readFileSync(`${__dirname}/data/bootcamp.json`, "utf-8")
 );
+const courses = JSON.parse(
+  fs.readFileSync(`${__dirname}/data/course.json`, "utf-8")
+);
 
 // import into db
 const importData = async () => {
   try {
     await Bootcamp.create(bootcamps);
+    await Course.create(courses);
     console.log("Data imported...".green.inverse);
   } catch (err) {
     console.error(err);
@@ -31,6 +36,8 @@ const importData = async () => {
 const deleteData = async () => {
   try {
     await Bootcamp.deleteMany();
+    await Course.deleteMany();
+
     console.log("Data destroyed...".red.inverse);
     process.exit();
   } catch (err) {
